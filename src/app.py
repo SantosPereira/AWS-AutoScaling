5ed0aeaa524ad16d346cda553b9dbe4bb0686ca5
SantosPereira <pedrohenriquelemam@gmail.com>
2022-01-12 13:37:59 -0300

    Modificações de estilização; Add Tela de resposta da req

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from cpu_load_generator import load_single_core, load_all_cores, from_profile
 
 app = Flask(__name__)
@@ -15,4 +15,11 @@ def geraCarga():
     # load_single_core(core_num=0, duration_s=20, target_load=0.4)  # generate load on single core (0)
     load_all_cores(duration_s=duracao, target_load=taxaConsumo)  # generates load on all cores
     # from_profile(path_to_profile_json=r"c:\profiles\profile1.json")
-    return f"Concluido\n\n {taxaConsumo}"
+
+    dados = {
+        'status':201
+    }
+    return jsonify(dados)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
