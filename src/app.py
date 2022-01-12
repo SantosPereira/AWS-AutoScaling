4a492c0fea7e3f26de946099517575f4f0c93049
SantosPereira <pedrohenriquelemam@gmail.com>
2022-01-12 11:32:20 -0300

    Começo do projeto; Mobile first

---
+++ src/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request
+from cpu_load_generator import load_single_core, load_all_cores, from_profile
+
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return render_template('index.html')
+
+
+@app.route("/carga", methods=["POST"])
+def geraCarga():
+    taxaConsumo = float(request.form['taxaConsumo'])
+    duracao = int(request.form['duracao'])
+    # load_single_core(core_num=0, duration_s=20, target_load=0.4)  # generate load on single core (0)
+    load_all_cores(duration_s=duracao, target_load=taxaConsumo)  # generates load on all cores
+    # from_profile(path_to_profile_json=r"c:\profiles\profile1.json")
+    return f"Concluido\n\n {taxaConsumo}"
