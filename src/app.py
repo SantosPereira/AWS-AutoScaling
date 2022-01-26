974b9913aa441e2d07869ea505e993ef5f32423c
SantosPereira <pedrohenriquelemam@gmail.com>
2022-01-26 11:41:22 -0300

    dfgsfd324

--- src/app.py
+++ src/app.py
@@ -1,25 +1,27 @@
-from flask import Flask, render_template, request, jsonify
-from cpu_load_generator import load_single_core, load_all_cores, from_profile
+from flask import Flask, request, jsonify, make_response
+from cpu_load_generator import load_all_cores
 
 app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return render_template('index.html')
+    return "Back-end dentro do Load Balancer"
+    # return render_template('index.html')
 
 
 @app.route("/carga", methods=["POST"])
 def geraCarga():
+    resposta = make_response(
+        dados = {
+            'status':201
+        }
+    )
+    resposta.headers['Access-Control-Allow-Origin'] = '*'
     taxaConsumo = float(request.form['taxaConsumo'])
     duracao = int(request.form['duracao'])
-    # load_single_core(core_num=0, duration_s=20, target_load=0.4)  # generate load on single core (0)
-    load_all_cores(duration_s=duracao, target_load=taxaConsumo)  # generates load on all cores
-    # from_profile(path_to_profile_json=r"c:\profiles\profile1.json")
+    load_all_cores(duration_s=duracao, target_load=taxaConsumo)
 
-    dados = {
-        'status':201
-    }
-    return jsonify(dados)
+    return resposta
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
